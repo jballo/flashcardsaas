86700fdb1fe439b88d4a956eddcf1945102670d6
Paradox560 <65742681+Paradox560@users.noreply.github.com>
2024-08-21 10:18:50 -0400

    changed button links and headers of landing, signup pages, signin pages

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,21 +1,50 @@
 import React from 'react'
 import { Container, Box, Typography, AppBar, Toolbar, Button } from '@mui/material'
-import { SignIn } from '@clerk/nextjs'
+import { SignIn, SignUp } from '@clerk/nextjs'
 import Link from 'next/link'
+import { Anton } from "next/font/google";
+import Header from "../../components/Header";
 
-export default function SignUpPage() {
+const anton = Anton({
+    weight: '400',
+    style: 'normal',
+    subsets: ['latin'],
+})
+
+export default function SignInPage() {
   return (
-    <Container>
-        <AppBar position="static" sx={{backgroundColor: '#3f51b5'}}>
-            <Toolbar>
-                <Typography variant="h6" sx={{flexGrow: 1}}>
-                Flashcard SaaS
-                </Typography>
-                <Button color="inherit">
-                <Link href="/login" passHref>
-                    Login
-                </Link>
-                </Button>
+    <Box>
+        <AppBar 
+            position="static"
+            left={0}
+            top={0}
+            sx={{
+                width: '100vw',
+                backgroundColor: '#62C4E1',
+            }}
+        >
+            <Toolbar
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                }}
+            >
+                <Box>
+                    <Button color='inherit' href="/" sx={{textTransform: 'none'}}>
+                        <Typography 
+                            variant="h5" 
+                            style={{flexGrow: 1}} 
+                            sx={{fontFamily: anton.style.fontFamily,
+                                letterSpacing: 1,
+                            }}
+                        >
+                            MindSpark
+                        </Typography>
+                    </Button>
+                </Box>
+                <Box>
+                    <Button color='inherit' href="/sign-up">Sign Up</Button>
+                </Box>
             </Toolbar>
         </AppBar>
         <Box
@@ -25,11 +54,11 @@ export default function SignUpPage() {
             alignItems="center"
             sx={{textAlign: 'center', my: 4}}
             >
-            <Typography variant="h4" component="h1" gutterBottom>
+            <Typography variant="h4" component="h1" gutterBottom marginBottom={5}>
                 Sign In
             </Typography>
             <SignIn />
         </Box>
-    </Container>
+    </Box>
   )
 }
