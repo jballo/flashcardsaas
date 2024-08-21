7be0c6294bd7761922390c0d8e0b82dcf04c2282
Paradox560 <65742681+Paradox560@users.noreply.github.com>
2024-08-21 14:51:40 -0400

    remodeled pricing page, stripe still doesn't work

--- app/contact/page.js
+++ app/contact/page.js
@@ -15,80 +15,80 @@ const teamMembers = [
     title: 'Full Stack Developer',
     description: 'Graduate of UC Merced',
     website: 'https://www.google.com',
-    imgUrl: '/w3images/team1.jpg',
+    imgUrl: "../../src/assets/leaf.jpeg",
   },
   {
     name: 'Karla Zamora',
     title: 'AI/ML and Backend Developer',
     description: 'University of Texas, Rio Grande Valley',
     website: 'https://www.google.com',
-    imgUrl: '/w3images/team2.jpg',
+    imgUrl: '/team2.jpg',
   },
   {
     name: 'Pranav Palle',
     title: 'Designer and Frontend Developer',
     description: 'University of Maryland, College Park',
     website: 'https://pranavpalle.netlify.app',
-    imgUrl: '/w3images/team3.jpg',
+    imgUrl: '/team3.jpg',
   },
 ];
 
 const ContactPage = () => {
   return (
     <Box
-      width="100vw"
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center'
-      }}
+        width="100vw"
+        sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            textAlign: 'center'
+        }}
     >
         <Header />
-    <Box sx={{ padding: 3 }} width="100vw">
-    <Typography 
-          variant="h1" 
-          gutterBottom
-          sx={{
-            fontSize: '5rem',
-            fontFamily: anton.style.fontFamily
-          }}
-        >
-          Meet The Team
-        </Typography>
-      <Grid container spacing={4}>
-        {teamMembers.map((member, index) => (
-          <Grid item xs={12} sm={6} md={4} key={index}>
-            <Card sx={{ boxShadow: 3 }}>
-              <CardMedia
-                component="img"
-                height="200"
-                image={member.imgUrl}
-                alt={member.name}
-              />
-              <CardContent>
-                <Typography variant="h5">{member.name}</Typography>
-                <Typography variant="body2" color="textSecondary" sx={{ mb: 2 }}>
-                  {member.title}
-                </Typography>
-                <Typography variant="body2">{member.description}</Typography>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  fullWidth
-                  sx={{ mt: 2 }}
-                  href={member.website}
-                  target="_blank"
+        <Box sx={{ padding: 3 }} width="100vw">
+            <Typography 
+                variant="h1" 
+                gutterBottom
+                sx={{
+                    fontSize: '5rem',
+                    fontFamily: anton.style.fontFamily
+                }}
                 >
-                  Personal Website
-                </Button>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
+                Meet The Team
+                </Typography>
+            <Grid container spacing={4}>
+                {teamMembers.map((member, index) => (
+                    <Grid item xs={12} sm={6} md={4} key={index}>
+                        <Card sx={{ boxShadow: 3 }}>
+                            <CardMedia
+                                component="img"
+                                style={{height: 0, paddingTop: '56.25%'}}
+                                image={member.imgUrl}
+                                alt={member.name}
+                            />
+                            <CardContent>
+                                <Typography variant="h5">{member.name}</Typography>
+                                <Typography variant="body2" color="textSecondary" sx={{ mb: 2 }}>
+                                    {member.title}
+                                </Typography>
+                                <Typography variant="body2">{member.description}</Typography>
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    fullWidth
+                                    sx={{ mt: 2 }}
+                                    href={member.website}
+                                    target="_blank"
+                                >
+                                    Personal Website
+                                </Button>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
     </Box>
   );
 };
--- app/page.js
+++ app/page.js
@@ -12,23 +12,6 @@ const anton = Anton({
 })
 
 export default function Home() {
-  const handleSubmit = async () => {
-    const checkoutSession = await fetch('/api/checkout_sessions', {
-      method: 'POST',
-      headers: { origin: 'http://localhost:3000' },
-    })
-    const checkoutSessionJson = await checkoutSession.json()
-  
-    const stripe = await getStripe()
-    const {error} = await stripe.redirectToCheckout({
-      sessionId: checkoutSessionJson.id,
-    })
-  
-    if (error) {
-      console.warn(error.message)
-    }
-  }
-
   return (
     <Box
       width="100vw"
