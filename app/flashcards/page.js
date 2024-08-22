e9c1cc612364298789e3652f77461b3a0f530c13
Karla-zamora <karla.zamora01@utrgv.edu>
2024-08-22 01:18:12 -0500

    can delete flashcard sets

--- app/flashcards/page.js
+++ app/flashcards/page.js
@@ -2,10 +2,10 @@
 
 import { useUser } from "@clerk/nextjs";
 import { useEffect, useState } from "react";
-import { collection, CollectionReference, deleteDoc, deleteField, doc, getDoc, getDocs, setDoc, updateDoc } from "firebase/firestore";
+import { collection, CollectionReference, deleteDoc, doc, getDoc, getDocs, setDoc, updateDoc } from "firebase/firestore";
 import { db } from "@/firebase";
 import { useRouter } from "next/navigation";
-import { AppBar, Box, Button, Card, CardActionArea, CardContent, Container, Grid, IconButton, Stack, TextField, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Card, CardActionArea, CardContent, Container, DialogContent, Dialog, DialogActions, DialogContentText, DialogTitle, Grid, IconButton, Stack, TextField, Toolbar, Typography } from "@mui/material";
 import { Anton } from "next/font/google";
 import LibraryAddIcon from '@mui/icons-material/LibraryAdd';
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -17,10 +17,13 @@ const anton = Anton({
 })
 
 export default function Flashcards() {
-  const { isLoaded, isSignedIn, user } = useUser()
-  const [flashcards, setFlashcards] = useState([])
-  const [filter, setFilter] = useState("")
-  // To trigger flashcard retrieval
+  const { isLoaded, isSignedIn, user } = useUser();
+  const [flashcards, setFlashcards] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [modal, setModal] = useState(false);
+  const [cardName, setCardName] = useState("");
+  // index to be deleted
+  const [delIndex, setDelIndex] = useState('')
   const [deleteOp, setdeleteOp] = useState('')
   const router = useRouter();
 
@@ -49,24 +52,38 @@ export default function Flashcards() {
   const handleCardClick = (id) => {
     router.push(`/flashcard?id=${id}`);
   };
+
   // Delete flashcard set
-  const handleDelete = async (index) => {
+  const handleDelete = async () => {
     // Get set reference in db
+    console.log(delIndex)
     const docRef = doc(collection(db, 'users'), user.id);
     const docSnap = await getDoc(docRef);
     const allCollections = docSnap.data().flashcards
-    const collectionToDelete = allCollections[index].name
+    const collectionToDelete = allCollections[delIndex].name
     // Delete from flashcards array in users.flashcards
-    allCollections.splice(index, 1)
+    allCollections.splice(delIndex, 1)
     await updateDoc(docRef, { flashcards: allCollections })
     // Delete collection in users (del all documents/flshcards)
     const flashcardsCollectionRef = collection(db, 'users', user.id, collectionToDelete);
     const flashcardsSnapshot = await getDocs(flashcardsCollectionRef);
     const deletePromises = flashcardsSnapshot.docs.map((doc) => deleteDoc(doc.ref));
     await Promise.all(deletePromises);
-    // Trigger retrieval of flashcards
+    // Trigger retrieval of flashcards and close modal
     setdeleteOp(collectionToDelete)
+    setModal(false)
   }
+
+  const handleOpen = (setname, index) => {
+    setModal(true);
+    setCardName(setname);
+    setDelIndex(index)
+  };
+
+  const handleClose = () => {
+    setModal(false);
+    setCardName("");
+  };
 
   return (
     <>
@@ -104,13 +121,12 @@ export default function Flashcards() {
           </Box>
         </Toolbar>
       </AppBar>
-
       <Box
         display="flex"
         flexDirection="column"
         justifyContent="center"
         alignItems="center"
-        sx={{ textAlign: 'center', my: 4, }}
+        sx={{ textAlign: 'center', my: 4 }}
       >
         <Typography
           variant="h1"
@@ -128,8 +144,8 @@ export default function Flashcards() {
         <Grid container spacing={3} sx={{ mt: 4 }}>
           {flashcards.map((flashcard, index) => (
             <Grid item xs={12} md={4} key={index}>
-              <Stack direction={'row'} gap={2} sx={{ justifyContent: 'space-evenly' }}>
-                <Card>
+              <Card>
+                <Stack direction={'row'}>
                   <CardActionArea onClick={() => handleCardClick(flashcard.name)}>
                     <CardContent>
                       <Stack direction={'row'} gap={3}>
@@ -138,16 +154,38 @@ export default function Flashcards() {
                       </Stack>
                     </CardContent>
                   </CardActionArea>
-                </Card>
-                <IconButton onClick={(e) => handleDelete(index)} sx={{ background: 'red' }}>
-                  <DeleteIcon style={{ color: 'white' }} />
-                </IconButton>
-              </Stack>
+                  <Box sx={{ alignContent: "center", justifyItems: "center", paddingLeft: 2, paddingRight: 2 }}>
+                    <IconButton aria-label="delete" onClick={() => handleOpen(flashcard.name, index)} sx={{ background: 'white', "&:hover": { bgcolor: "red" } }}>
+                      <DeleteIcon sx={{ color: 'black', "&:hover": { color: "white" } }} />
+                    </IconButton>
+                  </Box>
+                </Stack>
+              </Card>
             </Grid>
           ))}
         </Grid>
       </Container>
-
+      <Dialog
+        open={modal}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          Delete {cardName} Set
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to delete this set?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions sx={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Button onClick={handleClose}><Typography>No - Cancel</Typography></Button>
+          <Button onClick={handleDelete} autoFocus>
+            <Typography>Yes - Delete</Typography>
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
--- app/generate/page.js
+++ app/generate/page.js
@@ -28,14 +28,19 @@ export default function Generate() {
   const [amount, setAmount] = useState('10')
 
   const handleSubmit = async () => {
-    fetch('api/generate', {
-      method: 'POST',
-      body: "Topic: " + text + ", Difficulty: " + difficulty + ", Amount: " + amount,
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setFlashcards(data);
+    try {
+      fetch('api/generate', {
+        method: 'POST',
+        body: "Topic: " + text + ", Difficulty: " + difficulty + ", Amount: " + amount,
       })
+        .then((res) => res.json())
+        .then((data) => {
+          setFlashcards(data);
+        })
+    } catch(e) {
+      console.log(e)
+      alert("AI limit reached. Try again in a minute")
+    }
   };
 
   const handleCardClick = (id) => {
@@ -66,7 +71,7 @@ export default function Generate() {
         alert('Flashcard collection with the same name already exists.');
         return;
       } else {
-        collections.push({ name:name, difficulty:difficulty });
+        collections.push({ name: name, difficulty: difficulty });
         batch.set(userDocRef, { flashcards: collections }, { merge: true });
       }
     } else {
@@ -88,11 +93,11 @@ export default function Generate() {
     <Box
       width="100vw"
       sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          textAlign: 'center'
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center'
       }}
     >
       <AppBar
@@ -129,179 +134,181 @@ export default function Generate() {
           </Box>
         </Toolbar>
       </AppBar>
-        <Box 
-          width='75vw'
+      <Box
+        width='75vw'
+        sx={{
+          mt: 4,
+          mb: 6,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography
+          variant="h1"
+          gutterBottom
           sx={{
-            mt: 4,
-            mb: 6,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            fontSize: '5rem',
+            fontFamily: anton.style.fontFamily
           }}
         >
-          <Typography 
-            variant="h1" 
-            gutterBottom
+          Generate Flashcards
+        </Typography>
+        <Paper sx={{ p: 4, width: '100%' }}>
+          <TextField
+            value={text}
+            onChange={((e) => setText(e.target.value))}
+            label='Enter Topic'
+            fullWidth
+            multiline
+            rows={4}
+            variant='outlined'
             sx={{
-                fontSize: '5rem',
-                fontFamily: anton.style.fontFamily
+              mb: 2,
             }}
-          >
-              Generate Flashcards
-          </Typography>
-          <Paper sx={{ p: 4, width: '100%' }}>
-            <TextField
-              value={text}
-              onChange={((e) => setText(e.target.value))}
-              label='Enter Topic'
-              fullWidth
-              multiline
-              rows={4}
-              variant='outlined'
-              sx={{
-                mb: 2,
-              }}
-            />
-            <Box sx={{ display:'flex', justifyContent: 'space-evenly'}} gap={2}>
-              <FormControl sx={{ flex: 1 }}>
-                <InputLabel id="difficulty-label"> <Typography>Difficulty</Typography> </InputLabel>
-                <Select
-                  labelId="diff-select-label"
-                  displayEmpty
-                  value={difficulty}
-                  label="Difficulty"
-                  onChange={((e) => setDifficulty(e.target.value))}
-                >
-                  <MenuItem value={"Easy"}>Easy</MenuItem>
-                  <MenuItem value={"Normal"}>Normal</MenuItem>
-                  <MenuItem value={"Hard"}>Hard</MenuItem>
-                  <MenuItem value={"Master"}>Master</MenuItem>
-                </Select>
-              </FormControl>
-              <FormControl sx={{ flex: 1 }}>
-                <InputLabel id="difficulty-label"> <Typography>Amount</Typography> </InputLabel>
-                <Select
-                  labelId="amnt-select-label"
-                  displayEmpty
-                  value={amount}
-                  label="Amount"
-                  onChange={((e) => setAmount(e.target.value))}
-                >
-                  <MenuItem value={"5"}>5</MenuItem>
-                  <MenuItem value={"10"}>10</MenuItem>
-                  <MenuItem value={"15"}>15</MenuItem>
-                  <MenuItem value={"20"}>20</MenuItem>
-                  <MenuItem value={"25"}>25</MenuItem>
-                  <MenuItem value={"30"}>30</MenuItem>
-                </Select>
-              </FormControl>
-              <Button sx={{ flex: 1 }} variant='contained' color='primary' onClick={handleSubmit} >
-                {' '}
-                Submit
+          />
+          <Box sx={{ display: 'flex', justifyContent: 'space-evenly' }} gap={2}>
+            <FormControl sx={{ flex: 1 }}>
+              <InputLabel id="difficulty-label"> <Typography>Difficulty</Typography> </InputLabel>
+              <Select
+                labelId="diff-select-label"
+                required
+                displayEmpty
+                value={difficulty}
+                label="Difficulty"
+                onChange={((e) => setDifficulty(e.target.value))}
+              >
+                <MenuItem value={"Easy"}>Easy</MenuItem>
+                <MenuItem value={"Normal"}>Normal</MenuItem>
+                <MenuItem value={"Hard"}>Hard</MenuItem>
+                <MenuItem value={"Master"}>Master</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl sx={{ flex: 1 }}>
+              <InputLabel id="difficulty-label"> <Typography>Amount</Typography> </InputLabel>
+              <Select
+                labelId="amnt-select-label"
+                required
+                displayEmpty
+                value={amount}
+                label="Amount"
+                onChange={((e) => setAmount(e.target.value))}
+              >
+                <MenuItem value={"5"}>5</MenuItem>
+                <MenuItem value={"10"}>10</MenuItem>
+                <MenuItem value={"15"}>15</MenuItem>
+                <MenuItem value={"20"}>20</MenuItem>
+                <MenuItem value={"25"}>25</MenuItem>
+                <MenuItem value={"30"}>30</MenuItem>
+              </Select>
+            </FormControl>
+            <Button sx={{ flex: 1 }} variant='contained' color='primary' onClick={handleSubmit} >
+              {' '}
+              Submit
+            </Button>
+          </Box>
+        </Paper>
+      </Box>
+
+      {flashcards.length > 0 && (
+        <>
+          <Box sx={{ mt: 4 }} width='75vw'>
+            <Typography variant='h4' sx={{ mb: 4 }}>Flashcards Preview</Typography>
+            <Grid container spacing={3}>
+              {
+                flashcards.map((flashcard, index) => (
+                  <Grid item xs={12} sm={6} md={4} key={index}>
+                    <Card>
+                      <CardActionArea onClick={() => handleCardClick(index)}>
+                        <CardContent>
+                          <Box
+                            sx={{
+                              perspective: '1000px',
+                              '& > div': {
+                                transition: 'transform 0.6s',
+                                transformStyle: 'preserve-3d',
+                                position: 'relative',
+                                width: '100%',
+                                height: '300px',
+                                boxShadow: '0 4px 4px 0 rgba(0, 0, 0, 0.2)',
+                                transform: flipped[index] ? 'rotateY(180deg)' : 'rotateY(0deg)',
+                              },
+                              '& > div > div': {
+                                position: 'absolute',
+                                width: '100%',
+                                height: '100%',
+                                backfaceVisibility: 'hidden',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                padding: 2,
+                                boxSizing: 'border-box',
+                              },
+                              '& > div > div:nth-of-type(2)': {
+                                transform: 'rotateY(180deg)',
+                              },
+                            }}
+                          >
+                            <div>
+                              <div>
+                                <Typography variant='h5' component='div'>
+                                  {flashcard.front}
+                                </Typography>
+                              </div>
+                              <div>
+                                <Typography variant='h6' component='div'>
+                                  {flashcard.back}
+                                </Typography>
+                              </div>
+                            </div>
+                          </Box>
+                        </CardContent>
+                      </CardActionArea>
+                    </Card>
+                  </Grid>
+                ))
+              }
+            </Grid>
+            <Box sx={{ my: 6, display: 'flex', justifyContent: 'center' }}>
+              <Button variant='contained' color='secondary' onClick={handleOpen}>
+                Save
               </Button>
             </Box>
-          </Paper>
-        </Box>
-
-        {flashcards.length > 0 && (
-          <>
-            <Box sx={{ mt: 4 }} width='75vw'>
-              <Typography variant='h4' sx={{mb: 4}}>Flashcards Preview</Typography>
-              <Grid container spacing={3}>
-                {
-                  flashcards.map((flashcard, index) => (
-                    <Grid item xs={12} sm={6} md={4} key={index}>
-                      <Card>
-                        <CardActionArea onClick={() => handleCardClick(index)}>
-                          <CardContent>
-                            <Box
-                              sx={{
-                                perspective: '1000px',
-                                '& > div': {
-                                  transition: 'transform 0.6s',
-                                  transformStyle: 'preserve-3d',
-                                  position: 'relative',
-                                  width: '100%',
-                                  height: '300px',
-                                  boxShadow: '0 4px 4px 0 rgba(0, 0, 0, 0.2)',
-                                  transform: flipped[index] ? 'rotateY(180deg)' : 'rotateY(0deg)',
-                                },
-                                '& > div > div': {
-                                  position: 'absolute',
-                                  width: '100%',
-                                  height: '100%',
-                                  backfaceVisibility: 'hidden',
-                                  display: 'flex',
-                                  justifyContent: 'center',
-                                  alignItems: 'center',
-                                  padding: 2,
-                                  boxSizing: 'border-box',
-                                },
-                                '& > div > div:nth-of-type(2)': {
-                                  transform: 'rotateY(180deg)',
-                                },
-                              }}
-                            >
-                              <div>
-                                <div>
-                                  <Typography variant='h5' component='div'>
-                                    {flashcard.front}
-                                  </Typography>
-                                </div>
-                                <div>
-                                  <Typography variant='h6' component='div'>
-                                    {flashcard.back}
-                                  </Typography>
-                                </div>
-                              </div>
-                            </Box>
-                          </CardContent>
-                        </CardActionArea>
-                      </Card>
-                    </Grid>
-                  ))
-                }
-              </Grid>
-              <Box sx={{ my: 6, display: 'flex', justifyContent: 'center' }}>
-                <Button variant='contained' color='secondary' onClick={handleOpen}>
-                  Save
-                </Button>
-              </Box>
-            </Box>
-            <Dialog
-              open={modal}
-              onClose={handleClose}
-              aria-labelledby="alert-dialog-title"
-              aria-describedby="alert-dialog-description"
-            >
-              <DialogTitle id="alert-dialog-title">
-                {"Save Flashcards"}
-              </DialogTitle>
-              <DialogContent>
-                <DialogContentText id="alert-dialog-description">
-                  Please enter a name for your flashcards collection
-                </DialogContentText>
-                <TextField
-                  autoFocus
-                  required
-                  fullWidth
-                  margin='dense'
-                  label='Collection Name'
-                  type='text'
-                  value={name}
-                  onChange={((e) => setName(e.target.value))}
-                  variant='outlined'
-                />
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={handleClose}>Cancel</Button>
-                <Button onClick={saveFlashcards} autoFocus>
-                  Save
-                </Button>
-              </DialogActions>
-            </Dialog>
-          </>
-        )}
+          </Box>
+          <Dialog
+            open={modal}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              {"Save Flashcards"}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Please enter a name for your flashcards collection
+              </DialogContentText>
+              <TextField
+                autoFocus
+                required
+                fullWidth
+                margin='dense'
+                label='Collection Name'
+                type='text'
+                value={name}
+                onChange={((e) => setName(e.target.value))}
+                variant='outlined'
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button onClick={saveFlashcards} autoFocus>
+                Save
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </>
+      )}
     </Box>
   );
 
